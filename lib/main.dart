b4d8619e8f65f42cfc48e3b0aa37ae2682418956
Brett Morgan <brettmorgan@google.com>
2024-05-06 11:51:52 +1000

    Add lints

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
